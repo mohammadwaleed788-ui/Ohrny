22ced2a6169052912cd794c50cef596fc54d8fd1
waleed <mohammadwaleed788@gmail.com>
2026-06-19 17:43:00 +0500

    added last_user_read_at in migrations

--- backend/drizzle/0017_support_last_user_read_at.sql
+++ backend/drizzle/0017_support_last_user_read_at.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "support_tickets" ADD COLUMN "last_user_read_at" timestamp with time zone;
+ALTER TABLE "support_tickets" ADD COLUMN IF NOT EXISTS "last_user_read_at" timestamp with time zone;
